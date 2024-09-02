824bb656c264ddc24d7c3d7d7e7e3356aa1625ae
Igor Milosavljevic <migor@live.co.uk>
2024-09-02 16:34:28 +0100

    Added ground rollout

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -1,10 +1,12 @@
-import { Canvas, Trigger } from "./types";
+import { Canvas, Terrain, Trigger } from "./types";
 import { Player } from "../entities/Player";
 
 class PlayScene extends Phaser.Scene {
   private player: Player;
+  private terrain: Terrain;
   private canvas: Canvas;
   private startTrigger: Trigger;
+  private beginRollOut: boolean = false;
 
   constructor() {
     super("PlayScene");
@@ -15,35 +17,49 @@ class PlayScene extends Phaser.Scene {
       height: +this.game.config.height,
       width: +this.game.config.width,
     };
-    this.startTrigger;
 
     this.createTerrain();
     this.createPlayer();
-    this.createTriggers();
-
-    //checking for collisions
-    this.physics.add.overlap(this.startTrigger, this.player, () => {
-      console.log("Collision detected.");
-    });
+    this.createCollisionTriggers();
   }
 
+  update() {
+    if (this.beginRollOut && this.terrain.width <= this.canvas.width) {
+      console.log("increasing terrain width...");
+      this.terrain.width += 5;
+    }
+  }
+
+  //custom methods
   createPlayer() {
     this.player = new Player(this, 0, this.canvas.height);
   }
 
   createTerrain() {
     //x, y, width, height, image name
-    this.add
-      .tileSprite(this.canvas.width, this.canvas.height, 1000, 26, "ground")
+    this.terrain = this.add
+      .tileSprite(0, this.canvas.height, 88, 26, "ground")
       .setOrigin(0, 1);
   }
 
-  createTriggers() {
+  createCollisionTriggers() {
     //making an empty sprite for start detection
     this.startTrigger = this.physics.add
       .sprite(0, 10, null)
       .setOrigin(0, 1)
       .setAlpha(0);
+
+    //checking for collisions
+    this.physics.add.overlap(this.startTrigger, this.player, () => {
+      if (this.startTrigger.y === 10) {
+        this.startTrigger.body.reset(0, this.canvas.height);
+        console.log("Hit");
+        return;
+      }
+
+      this.startTrigger.destroy();
+      this.beginRollOut = true;
+    });
   }
 }
 
--- src/scenes/types.ts
+++ src/scenes/types.ts
@@ -1,5 +1,6 @@
 //my custom types! :)
 export type Trigger = Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+export type Terrain = Phaser.GameObjects.TileSprite;
 
 export interface Canvas {
   height: number;
